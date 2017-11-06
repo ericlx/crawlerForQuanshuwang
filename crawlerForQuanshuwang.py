58686e858e6ea4fa3774d927f151e41e621e6c20
Eric <ericlxhk@gmail.com>
2017-11-06 18:42:08 +0800

    Update crawlerForQuanshuwang.py

--- crawlerForQuanshuwang.py
+++ crawlerForQuanshuwang.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+# Title: crawlerForQuanshuwang
+# Author: EricLX
+# Operating Environment: Python 2.X
+# Version: 1.0.6
+# Date: 06 November, 2017
 
 import urllib, urllib2
 import re
